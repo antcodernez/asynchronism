800bf04d3023d3cd7285f2fc24c2d99922d6746b
Antonio Jiménez <marshday5@gmail.com>
2023-09-09 13:10:31 -0600

    Update about fetch() in nodejs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # asynchronism
 This are my notes about the course asynchronism by platzi
+
+
+Now i can use fetch without librarys
+
--- src/asyc/challence.js
+++ src/asyc/challence.js
@@ -1,18 +1,18 @@
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 const API = "https://api.escuelajs.co/api/v1";
 
-//Lógica de async: ir por los datos, luego esperar por ellos y finalmente retornarlos hacia el usuario
+//Logica de async: ir por los datos, luego esperar por ellos y finalmente retornarlos hacia el usuario
 async function fetchData(urlApi) //siempre async antes de function
     {
         const response = await fetch(urlApi) //hacemos uso del fetch()
         const data = await response.json(); //estructura de los datos transformandolos en json
-        return data//retorna la información de la API que estamos solicitando
+        return data //retorna la informacion de la API que estamos solicitando
     }    
 
-//también se puede colocar la palabra async antes del argumento y se usa arrow function
+//tambien se puede colocar la palabra async antes del argumento y se usa arrow function
 const anotherFunction = async urlApi => 
     {
-        //En try estará todo lo que queremos que suceda con la lógica de la aplicación
+        //En try estara todo lo que queremos que suceda con la logica de la aplicacion
         try 
             {
                 const products = await fetchData(`${urlApi}/products`);
@@ -32,7 +32,7 @@ anotherFunction(API);
 
 // Cuando se ejecuta console.log(products); en fetchData se solicita todos los productos de la API con ${urlApi}/products es por eso que vemos en la salida una larga lista de bloques entre corchetes [] y separados por llaves {}.
 
-// Cuando se ejecuta console.log(product.title); en fetchData se solicita el título de un producto en particular accediendo al atributo title, para ello tenemos que con ${urlApi}/products/${products[0].id} el cero 0 indica la posición de products que acabamos de almacenar la lista de productos, pero debe finalizar con id porque esa es la forma para acceder al objeto con ese identificador en la posición cero.
-// Si solo colocamos ${urlApi}/products/${products[0]} sin el .id, en la consola tendríamos un error de tipo: TypeError: Cannot read properties of undefined (reading 'id') así que no podemos acceder.
+// Cuando se ejecuta console.log(product.title); en fetchData se solicita el titulo de un producto en particular accediendo al atributo title, para ello tenemos que con ${urlApi}/products/${products[0].id} el cero 0 indica la posicion de products que acabamos de almacenar la lista de productos, pero debe finalizar con id porque esa es la forma para acceder al objeto con ese identificador en la posicion cero.
+// Si solo colocamos ${urlApi}/products/${products[0]} sin el .id, en la consola tendriamos un error de tipo: TypeError: Cannot read properties of undefined (reading 'id') asi que no podemos acceder.
 
-// Cuando se ejecuta console.log(category.name); en fetchData se solicita el nombre de la categoría que corresponda al producto que se llamó anteriormente, para ello tenemos ${urlApi}/categories/${product.category.id} y no es necesario indicar la posición porque por cada producto, solo hay un bloque de categoría.
+// Cuando se ejecuta console.log(category.name); en fetchData se solicita el nombre de la categoria que corresponda al producto que se llamo anteriormente, para ello tenemos ${urlApi}/categories/${product.category.id} y no es necesario indicar la posicion porque por cada producto, solo hay un bloque de categoria.
